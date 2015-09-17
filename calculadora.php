7b254634ee9b0bd7ce4764aebd632d26489d2b7a
Paulo Chiozzani <chiozzani23@icloud>
2015-09-17 20:02:41 -0300

    implementacao do formulario da calculadora

---
+++ calculadora.php
@@ -0,0 +1,24 @@
+<php
+
+<html>
+
+    <br><br>
+    <div id="calculadora1">Calculadora:<br>
+        <form method="POST">
+            <label>Numero 1:</label>
+            <input type="number" name="Num1"/>
+            <label>Numero 2:</label>
+            <input type="number" name="Num2"/>
+            <label>Resultado:</label>
+            <input type="number" name="Res"/>
+
+            <input type="hidden" name="acao" value="calcular"/>
+            <input type="submit" name="btnsoma" value="Somar"/>
+                
+         </form>
+    </div>
+
+</html>
+
+
+?>
