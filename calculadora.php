2ec7ff0603b8ad14876f0d1991b34a38a938828d
Paulo Chiozzani <chiozzani23@icloud>
2015-09-17 20:24:25 -0300

    implementacao da subtracao no php

--- calculadora.php
+++ calculadora.php
@@ -11,8 +11,8 @@
             <input type="number" name="Num2"/>
             <label>Resultado:</label>
             <input type="number" name="Res"/>
-            <input type="submit" name="btnsoma" value="Somar"/>
-            <input type="submit" name="btnsoma" value="Subtrair"/>
+            <input type="submit" name="btncalc" value="Somar"/>
+            <input type="submit" name="btncalc" value="Subtrair"/>
 
 
                 
@@ -22,14 +22,27 @@
 </html>
 
 
-    if(isset($_POST['acao']) && $_POST['acao']=='soma'){
+    if(isset($_POST['btncalc']) && $_POST['btncalc']=='Somar'){
 
-    $acao = $_POST['acao'];
-    $num1 = $_POST['Num1'];
-    $num2 = $_POST['Num2'];
+       $acao = $_POST['btncalc'];
+       $num1 = $_POST['Num1'];
+       $num2 = $_POST['Num2'];
     
-    $res = $num1 + $num2;        
+       $res = $num1 + $num2;        
     
-    echo 'Resultado: '.$res
+       echo 'Resultado da operacao: '.$acao.' '.$res;
+    
+     }
 
+    if{isset($_POST['btncalc']) && $_POST['btncalc']=='Subtrair')
+
+       
+       $num1 = $_POST['Num1'];
+       $num2 = $_POST['Num2'];
+    
+       $res = $num1 - $num2;        
+
+       echo 'Resultado da operacao: '.$acao.' '.$res;
+     
+    }
 ?>
