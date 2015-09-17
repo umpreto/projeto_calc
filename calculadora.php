1876a5c98bfcf7f5987ebb6c14501a789255caa3
Paulo Chiozzani <chiozzani23@icloud>
2015-09-17 20:09:18 -0300

    Implementar php com Soma

--- calculadora.php
+++ calculadora.php
@@ -21,4 +21,14 @@
 </html>
 
 
+    if(isset($_POST['acao']) && $_POST['acao']=='calcular'){
+
+    $acao = $_POST['acao'];
+    $num1 = $_POST['Num1'];
+    $num2 = $_POST['Num2'];
+    
+    $res = $num1 + $num2;        
+    
+    echo 'Resultado: '.$res
+
 ?>
